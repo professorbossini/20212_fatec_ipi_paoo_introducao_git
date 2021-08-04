5938ee92d12d792f1b679db753aa9c571f6fbace
Rodrigo Bossini <professorbossini@gmail.com>
2021-08-04 13:45:10 -0300

    Commit inicial. Classe Empregado criada.

---
+++ Empregado.java
@@ -0,0 +1,20 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    public void setIdade (int idade){
+        this.idade = idade;
+    }
+}
